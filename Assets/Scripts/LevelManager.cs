e726af11e40244137ded95a9a69292b57b244804
CoderOrk <jasonbahr.jb@gmail.com>
2023-03-04 22:01:24 -0800

    Added music and basic navigation. Added all scenes w/ background

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadStart()
     {
-        
+        LoadScene(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadMenu()
     {
-        
+        LoadScene(1);
+    }
+
+    public void LoadOptions()
+    {
+        LoadScene(2);
+    }
+
+        public void LoadTraverse()
+    {
+        LoadScene(3);
+    }
+
+    public void LoadDestruction()
+    {
+        LoadScene(4);
+    }
+
+        public void LoadCriticalHit()
+    {
+        LoadScene(5);
+    }
+
+        public void LoadSpaceStation()
+    {
+        LoadScene(6);
+    }
+
+        public void LoadBattle()
+    {
+        LoadScene(7);
+    }
+
+    void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 }
